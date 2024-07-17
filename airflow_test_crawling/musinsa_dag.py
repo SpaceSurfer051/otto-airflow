15aa655754386baa29e4c9e8169299bb86def671
papalio <dldudwns887@naver.com>
2024-07-17 16:51:22 +0900

    airflow_사전준비

--- airflow_test_crawling/musinsa_dag.py
+++ airflow_test_crawling/musinsa_dag.py
@@ -1,17 +1,4 @@
-'''
-task 순서
 
-
-실행 주기 : (하루에 한 번 정도가 적당한 것 같음)
-
-1. review_product_data_crawling.py - review와 product 정보 크롤링
-
-2. color_size_crawling.py - 컬러와 사이즈 정보 크롤링
-
-3. 데이터 전처리 ( 예정 )
-
-
-'''
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
@@ -35,13 +22,13 @@ dag = DAG(
 )
 
 def run_review_product_data_crawling():
-    subprocess.run(["python", "/path/to/review_product_data_crawling.py"], check=True)
+    subprocess.run(["python", "/path/to/airflow_product_review.py"], check=True)
 
 def run_color_size_crawling():
-    subprocess.run(["python", "/path/to/color_size_crawling.py"], check=True)
+    subprocess.run(["python", "/path/to/airflow_size_color.py"], check=True)
 
 def run_test2():
-    subprocess.run(["python", "/path/to/test2.py"], check=True)
+    subprocess.run(["python", "/path/to/airflow_data_preprocessing.py"], check=True)
 
 t1 = PythonOperator(
     task_id='review_product_data_crawling',
