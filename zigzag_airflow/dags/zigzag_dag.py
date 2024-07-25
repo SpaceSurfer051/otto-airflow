b14d43d323cadaf79708db19212e928f0654232a
1000zoo <cjswldn99@gmail.com>
2024-07-25 12:33:36 +0900

    docs: 주석 추가

--- zigzag_airflow/dags/zigzag_dag.py
+++ zigzag_airflow/dags/zigzag_dag.py
@@ -1,20 +1,16 @@
 from airflow import DAG
-from airflow.hooks.S3_hook import S3Hook
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
-from airflow.decorators import task
-
+from airflow.hooks.S3_hook import S3Hook
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-from zigzag_crawling import get_product_id, product_crawling, review_crawling
-
-import pandas as pd
-import logging
-
 from io import StringIO
 
+import logging
+import pandas as pd
 
+
+# 웹 드라이버를 생성하는 함수
 def get_driver():
     logging.info("creating driver.")
     options = Options()
@@ -29,22 +25,22 @@ def get_driver():
     return driver
 
 
+# S3에서 CSV 파일을 읽어오는 함수
 def get_csv_from_s3(bucket_name, key):
     try:
         s3_hook = S3Hook(aws_conn_id="aws_s3")
-
         s3_client = s3_hook.get_conn()
         obj = s3_client.get_object(Bucket=bucket_name, Key=key)
         df = pd.read_csv(StringIO(obj["Body"].read().decode("utf-8-sig")))
         logging.info(f"successfully get csv file from {key}")
         logging.info(f"loaded csv length ::: {len(df)}")
-
         return df
     except Exception as e:
         logging.info(e)
         return pd.DataFrame()
 
 
+# 데이터프레임을 S3에 저장하는 함수
 def save_df_to_s3(df, bucket_name, key):
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=False, encoding="utf-8-sig")
@@ -52,19 +48,23 @@ def save_df_to_s3(df, bucket_name, key):
     s3_hook.load_string(csv_buffer.getvalue(), key, bucket_name, replace=True)
 
 
+# 데이터프레임에 순위를 설정하는 함수
 def set_rank(df, sorted_product_list):
     df["rank"] = "none"
     rank_dict = {
         product_id: rank + 1 for rank, product_id in enumerate(sorted_product_list)
     }
     df["rank"] = df["product_id"].map(rank_dict).fillna("none")
-
     return df
 
 
+# 크롤링 데이터를 업데이트하는 함수
 def update_crawling_data(bucket_name, product_max_num=10, review_max_num=20):
+    # 크롤링에 필요한 URL 및 카테고리 ID 설정
     products_url = "https://zigzag.kr/categories/-1?title=%EC%9D%98%EB%A5%98&category_id=-1&middle_category_id={id}&sort=201"
     category_ids = {"top": "474", "bottom": "547"}
+
+    # S3에서 기존 데이터 가져오기
     product_df = get_csv_from_s3(bucket_name, "non-integrated-data/zigzag_products.csv")
     review_df = get_csv_from_s3(bucket_name, "non-integrated-data/zigzag_reviews.csv")
     link_set = set(product_df["product_url"])
@@ -103,6 +103,7 @@ def update_crawling_data(bucket_name, product_max_num=10, review_max_num=20):
     driver.quit()
 
 
+# Airflow DAG 설정
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
@@ -120,6 +121,7 @@ dag = DAG(
     tags=["update_dag"],
 )
 
+# PythonOperator를 이용하여 update_crawling_data 함수 호출
 update_task = PythonOperator(
     task_id="update_crawling_data",
     python_callable=update_crawling_data,
