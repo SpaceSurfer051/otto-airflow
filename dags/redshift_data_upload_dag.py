1191f63e9208ff0b9d0bfbd243d15b4f20502af0
papalio <dldudwns887@naver.com>
2024-07-31 17:06:20 +0900

    rds_taskgroup_up

--- dags/redshift_data_upload_dag.py
+++ dags/redshift_data_upload_dag.py
@@ -3,6 +3,7 @@ from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 import redshift_tasks
 import rds_tasks
+from airflow.utils.task_group import TaskGroup
 
 default_args = {
     'owner': 'airflow',
@@ -17,64 +18,62 @@ dag = DAG(
     schedule_interval=None,
 )
 
-create_schema_task = PythonOperator(
-    task_id='create_schema',
-    python_callable=redshift_tasks.create_schema,
-    dag=dag,
-)
+with dag:
+    with TaskGroup('redshift_part', tooltip='redshift_data_upload_part') as redshift_part:
+        create_schema_task = PythonOperator(
+            task_id='create_schema',
+            python_callable=redshift_tasks.create_schema,
+        )
 
-create_tables_task = PythonOperator(
-    task_id='create_tables',
-    python_callable=redshift_tasks.create_tables,
-    dag=dag,
-)
+        create_tables_task = PythonOperator(
+            task_id='create_tables',
+            python_callable=redshift_tasks.create_tables,
+        )
 
-upload_product_data_task = PythonOperator(
-    task_id='upload_product_data',
-    python_callable=redshift_tasks.upload_product_data,
-    provide_context=True,
-    dag=dag,
-)
+        upload_product_data_task = PythonOperator(
+            task_id='upload_product_data',
+            python_callable=redshift_tasks.upload_product_data,
+            provide_context=True,
+        )
 
-read_review_data_task = PythonOperator(
-    task_id='read_review_data',
-    python_callable=redshift_tasks.read_review_data,
-    provide_context=True,
-    dag=dag,
-)
+        read_review_data_task = PythonOperator(
+            task_id='read_review_data',
+            python_callable=redshift_tasks.read_review_data,
+            provide_context=True,
+        )
 
-get_existing_product_names_task = PythonOperator(
-    task_id='get_existing_product_names',
-    python_callable=redshift_tasks.get_existing_product_names,
-    provide_context=True,
-    dag=dag,
-)
+        get_existing_product_names_task = PythonOperator(
+            task_id='get_existing_product_names',
+            python_callable=redshift_tasks.get_existing_product_names,
+            provide_context=True,
+        )
 
-process_and_upload_review_data_task = PythonOperator(
-    task_id='process_and_upload_review_data',
-    python_callable=redshift_tasks.process_and_upload_review_data,
-    provide_context=True,
-    dag=dag,
-)
+        process_and_upload_review_data_task = PythonOperator(
+            task_id='process_and_upload_review_data',
+            python_callable=redshift_tasks.process_and_upload_review_data,
+            provide_context=True,
+        )
 
-identify_max_lengths_task = PythonOperator(
-    task_id='identify_max_lengths',
-    python_callable=redshift_tasks.identify_max_lengths,
-    provide_context=True,
-    dag=dag,
-)
+        identify_max_lengths_task = PythonOperator(
+            task_id='identify_max_lengths',
+            python_callable=redshift_tasks.identify_max_lengths,
+            provide_context=True,
+        )
 
-create_rds_tables_task = PythonOperator(
-    task_id='create_rds_tables',
-    python_callable=rds_tasks.create_rds_tables,
-    dag=dag,
-)
+        create_schema_task >> create_tables_task >> upload_product_data_task >> read_review_data_task >> get_existing_product_names_task >> identify_max_lengths_task >> process_and_upload_review_data_task
 
-transfer_data_to_rds_task = PythonOperator(
-    task_id='transfer_data_to_rds',
-    python_callable=rds_tasks.transfer_data_to_rds,
-    provide_context=True,
-    dag=dag,
-)
+    with TaskGroup('rds_part', tooltip='rds_part') as rds_part:
+        create_rds_tables_task = PythonOperator(
+            task_id='create_rds_tables',
+            python_callable=rds_tasks.create_rds_tables,
+        )
 
-create_schema_task >> create_tables_task >> upload_product_data_task >> read_review_data_task >> get_existing_product_names_task >> identify_max_lengths_task >> process_and_upload_review_data_task >> create_rds_tables_task >> transfer_data_to_rds_task
+        transfer_data_to_rds_task = PythonOperator(
+            task_id='transfer_data_to_rds',
+            python_callable=rds_tasks.transfer_data_to_rds,
+            provide_context=True,
+        )
+
+        create_rds_tables_task >> transfer_data_to_rds_task
+
+    redshift_part >> rds_part
