7093337867bf02104d9bca2035616b1463ec634e
papalio <dldudwns887@naver.com>
2024-08-01 18:22:23 +0900

    test

--- dags/redshift_data_upload_dag.py
+++ dags/redshift_data_upload_dag.py
@@ -12,7 +12,7 @@ default_args = {
 }
 
 dag = DAG(
-    #test 24_7
+    #test 24_8
     'otto_redshift_data_upload_real_24_7',
     default_args=default_args,
     description='Upload product and review data to Redshift with deduplication and move data to RDS',
