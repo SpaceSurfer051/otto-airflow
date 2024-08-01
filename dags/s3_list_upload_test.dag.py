94c6df32d2093c87a830f4fdd5c6b454f185c1a1
papalio <dldudwns887@naver.com>
2024-08-01 14:05:23 +0900

    test

--- dags/s3_list_upload_test.dag.py
+++ dags/s3_list_upload_test.dag.py
@@ -3,14 +3,14 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.hooks.S3_hook import S3Hook
 from datetime import datetime
 
-def list_product_in_s3(bucket_name, prefix):
+def list_product_in_s3(bucket_name, prefix_product,prefix_review):
     s3_hook = S3Hook(aws_conn_id='aws_default')
-    files_product = s3_hook.list_keys(bucket_name=bucket_name, prefix=prefix)
+    files_product = s3_hook.list_keys(bucket_name=bucket_name, prefix=prefix_product)
     files_product2 = files_product[-1]
     print(files_product2)
     
     
-    files_review = s3_hook.list_keys(bucket_name=bucket_name, prefix=prefix)
+    files_review = s3_hook.list_keys(bucket_name=bucket_name, prefix=prefix_review)
     files_review2 = files_review[-1]
     print(files_review2)
     
@@ -33,7 +33,8 @@ with DAG('s3_list_files_dag',
         python_callable=list_product_in_s3,
         op_kwargs={
             'bucket_name': 'otto-glue',
-            'prefix': 'integrated-data/products/'
+            'prefix_product': 'integrated-data/products/',
+            'prefix_review': 'integrated-data/reviews/'
         },
     )
 
