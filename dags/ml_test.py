0f85306d8d700262392b75c965982c8fbb211aab
spacesurfer051 <csb360@naver.com>
2024-08-13 23:51:34 +0900

    fix
    fix

--- dags/ml_test.py
+++ dags/ml_test.py
@@ -1,20 +1,22 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.postgres_operator import PostgresOperator
 from airflow.hooks.postgres_hook import PostgresHook
 from datetime import datetime
 
+# UNLOAD 쿼리를 생성하는 함수
 def generate_unload_query(**kwargs):
     redshift_hook = PostgresHook(postgres_conn_id='otto_redshift')
+    
+    # 컬럼명 추출 쿼리
     column_query = """
-        SELECT STRING_AGG(column_name, ', ')
+        SELECT STRING_AGG(column_name::text, ', ')
         FROM information_schema.columns
-        WHERE table_name = 'otto.ml_table';
+        WHERE table_name = 'ml_table' AND table_schema = 'otto';
     """
     
     # 컬럼명 추출
     columns = redshift_hook.get_first(sql=column_query)[0]
-
+    
     # 컬럼명 포함한 UNLOAD 쿼리 생성
     unload_query = f"""
     UNLOAD ('
@@ -22,18 +24,19 @@ def generate_unload_query(**kwargs):
         UNION ALL
         SELECT {columns} FROM otto.ml_table
     ')
-    TO 's3://'otto-ml-1'/'ml_table.csv'/'
-    ACCESS_KEY_ID 'your_access_key_id'
+    TO 's3://otto-ml-1/ml_table.csv'
     IAM_ROLE 'arn:aws:iam::862327261051:role/service-role/AmazonRedshift-CommandsAccessRole-20240726T150655'
     ADDQUOTES
     ALLOWOVERWRITE
     PARALLEL OFF;
     """
     
-    return unload_query
+    # 쿼리를 XCom에 저장
+    kwargs['ti'].xcom_push(key='unload_query', value=unload_query)
 
+# UNLOAD 쿼리를 실행하는 함수
 def execute_unload(**kwargs):
-    unload_query = kwargs['ti'].xcom_pull(task_ids='generate_unload_query')
+    unload_query = kwargs['ti'].xcom_pull(task_ids='generate_unload_query', key='unload_query')
     redshift_hook = PostgresHook(postgres_conn_id='otto_redshift')
     redshift_hook.run(unload_query)
 
@@ -50,6 +53,7 @@ dag = DAG(
     schedule_interval='@daily',
 )
 
+# UNLOAD 쿼리를 생성하는 작업
 generate_query = PythonOperator(
     task_id='generate_unload_query',
     python_callable=generate_unload_query,
@@ -57,6 +61,7 @@ generate_query = PythonOperator(
     dag=dag,
 )
 
+# UNLOAD 쿼리를 실행하는 작업
 execute_unload = PythonOperator(
     task_id='execute_unload',
     python_callable=execute_unload,
@@ -64,4 +69,5 @@ execute_unload = PythonOperator(
     dag=dag,
 )
 
+# 작업 순서 지정
 generate_query >> execute_unload
